9ac1f51378046d414e2adf26cceafc7732e2388e
naftalimurgor <murgornaftali@gmail.com>
2023-03-15 19:31:44 +0300

    update top level imports

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import * as lib from './lib';
 
 import * as cli from './cli';
 
+// utils
+// bgl
+// accounts
+// network- copy the entire web3 structure
 const web3 = { lib, cli };
 export default web3;
