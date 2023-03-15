40e0dd26d5d828ceaeb8293658c58f3d352f2423
naftalimurgor <murgornaftali@gmail.com>
2023-03-15 19:32:15 +0300

    add cli top level export

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,11 +4,28 @@ import chalk from 'chalk';
 import { Command } from 'commander';
 import jsbgl from '../lib';
 
+// itinerary:
+// 1. hookup mvp cli
+// 2. types for jsbgl
+// 3. currently testnet is supported, override with {testnet: true} and among other options
+// 4. test(with coverage report)
+// 5. demo(browser/nodejs) - would be useful to show usage
+// 5. publish to npm
+// 6. Report on discord and get paid
+
+/**
+ * `main` entry function for running the cli portion
+ * All the commandline nitty-gritties are handled by @link https://github.com/tj/commander.js
+ * Exposes:
+ * - Wallet functionality
+ * - Transactions functionality
+ * - Address functionality
+ */
 async function main() {
   try {
     await jsbgl.asyncInit(globalThis);
   } catch (error) {
-    chalk.yellow('E: Filed to initialize with', error);
+    chalk.yellow('E: Failed to initialize with', error);
   }
 
   const testnetRegegx = /^(Testnet|tesnet|TestNet|test net|testNet)$/i;
